21500afb77fce1f25321b691804e814d190956f7
rafiul-islam <rafiulrony.cse001@gmail.com>
2023-06-01 13:47:50 +0600

    test commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [ProductController::class, 'products'])->name('products');
+Route::post('/add-product', [ProductController::class, 'addProduct'])->name('add.product');
